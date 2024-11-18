b64b61be1d6259a31ff9eb1b26fd030be22cf31d
Dang <56210432+looks32@users.noreply.github.com>
2024-11-18 19:16:42 +0900

    데이터 바인딩 기초

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4 :style="스타일">원룸</h4>
+    <p>{{price1}} 만원</p>
+    <!-- asds -->
+  </div>
+  <div>
+    <h4>원룸</h4>
+    <p>{{price2}} 만원</p>
+  </div>
 </template>
 
 <script>
@@ -8,6 +17,13 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      price1 : 60,
+      price2 : 50,
+      스타일 : 'color : blue'
+     }
+  },
   components: {
     HelloWorld
   }
